1085c5f29b95b8450e3d756461f5d01273d64cbc
Tomas Kracmar <tomas.kracmar@cqre.net>
2022-04-08 10:36:20 +0200

    IP change

--- access_data.php
+++ access_data.php
@@ -2,7 +2,7 @@
 // ============================================================================
 // => ACCESS TO DB
 
-$sql_server = "localhost";
+$sql_server = "116.203.255.49";
 $sql_user 	= "dogotron";			// user name
 $sql_pwd  	= "dXmBeofHQANKz6VZ";	// user password
 $sql_db   	= "bezime";				// main DB
